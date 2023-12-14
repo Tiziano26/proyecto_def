3a8834d2c1a816fded3f75f53886e4283f523645
Tiziano26 <132932851+Tiziano26@users.noreply.github.com>
2023-12-14 06:50:34 -0300

    toda la pagina

---
+++ html/nosotros.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/nosotros.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark" style="border: 1px solid black;">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="index.php">GymShark</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="rutinas.php">Rutinas</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Productos
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="proteina.php">Proteinas</a></li>
+                      <li><a class="dropdown-item" href="aminoacidos.php">Aminoacidos</a></li>
+                      <li><a class="dropdown-item" href="multivitaminicos.php">Multivitaminicos</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="accesorios.php">Accesorios</a></li>
+                    </ul>
+                </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+            <div class="carrito">
+                <li><a href="">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAN1JREFUSEvNlN0RATEUhb/thE7ohFeKQBG8ohI6oRPmmM1Mdjeb3E3EuK9Jzpdz/xoqR1NZn58CXq2bJ3AF9t9w5ztwAKe7BO6lkFCKVsC5FRekKEKAGfBoVYtdjBVZDuQkN1THuR6PAXwXOZAkQKI3YJGjDhxcF8bmwBU7h6H0yEV00HLTdAHW7lepSdaw7SZakLggn0gBclx0NFOAqcXupMfiQHfUSeooSwwG0+JAwpZaaG8NVosVYPl98I4VcAQ2wAnY9pRiZ8kuclr+Ku9/KnZmBlR3UL0G/wt4A6frJhn9OGNiAAAAAElFTkSuQmCC"/>
+                </a></li>
+            </div>
+          </nav>
+        </header>
+        <section>
+        <div class="card" style="width: 18rem;">
+            <img src="imagenes/lucas.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Lucas Diaz</h5>
+              <p class="card-text">Scrum Master del equipo</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Sentadilla: 20kg</li>
+              <li class="list-group-item">Press banca: 15 kg</li>
+              <li class="list-group-item">Jalon al pecho: 25kg</li>
+            </ul>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="imagenes/kw.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Gabriel Colonia</h5>
+              <p class="card-text">Diseñador y programador del equipo</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Sentadilla: 20kg</li>
+              <li class="list-group-item">Press banca: 15 kg</li>
+              <li class="list-group-item">Jalon al pecho: 25kg</li>
+            </ul>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="imagenes/clmamado.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Tiziano Oviedo</h5>
+              <p class="card-text">Diseñador y programador del equipo</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Sentadilla: 20kg</li>
+              <li class="list-group-item">Press banca: 15 kg</li>
+              <li class="list-group-item">Jalon al pecho: 25kg</li>
+            </ul>
+          </div>
+        </section>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ html/rutinas.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/rutinas.css">
+</head>
+
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark" style="border: 1px solid black;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php">GymShark</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Rutinas</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Productos
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="proteina.php">Proteinas</a></li>
+                  <li><a class="dropdown-item" href="aminoacidos.php">Aminoacidos</a></li>
+                  <li><a class="dropdown-item" href="multivitaminicos.php">Multivitaminicos</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="accesorios.php">Accesorios</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php">Nosotros</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+        <div class="carrito">
+            <li><a href="">
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAN1JREFUSEvNlN0RATEUhb/thE7ohFeKQBG8ohI6oRPmmM1Mdjeb3E3EuK9Jzpdz/xoqR1NZn58CXq2bJ3AF9t9w5ztwAKe7BO6lkFCKVsC5FRekKEKAGfBoVYtdjBVZDuQkN1THuR6PAXwXOZAkQKI3YJGjDhxcF8bmwBU7h6H0yEV00HLTdAHW7lepSdaw7SZakLggn0gBclx0NFOAqcXupMfiQHfUSeooSwwG0+JAwpZaaG8NVosVYPl98I4VcAQ2wAnY9pRiZ8kuclr+Ku9/KnZmBlR3UL0G/wt4A6frJhn9OGNiAAAAAElFTkSuQmCC"/>
+            </a></li>
+        </div>
+      </nav>
+    </header>
+
+
+    <h1 class="titulo">Rutinas</h1>
+
+    <p class="infotitulo">¿Que son las Rutinas?<br></br>Las rutinas sirven para mejorar la salud física y mental, aumentar la fuerza, la resistencia y la flexibilidad, y contribuir a un estilo de vida activo y equilibrado.</br>La elección de la rutina depende de los objetivos individuales, ya sea mejorar la salud general, ganar músculo, perder peso o simplemente mantenerse en forma.</p>
+
+
+    <h4 class="famosas">Algunas rutinas famosas</h4>
+
+
+    <h1 class="heavy">Heavy Duty</h1>
+    <p class="infoheavy">La rutina Heavy Duty de Mike Mentzer es un enfoque de entrenamiento diseñado para la eficiencia y la máxima estimulación muscular en un corto período de tiempo.</br>Desarrollada por el culturista y entrenador Mike Mentzer, esta rutina se basa en principios de alta intensidad y volumen bajo.</p>
+    <h5 class="heavy2">Caracteristicas</h5>
+  
+    <p class="infoheavy2">Intensidad Extrema: La rutina se centra en ejercicios intensos y de alta intensidad con el objetivo de estimular al máximo las fibras musculares. Cada serie se realiza hasta el fallo muscular, lo que significa que se levanta el peso hasta que no se puede completar otra repetición con buena forma.</br>
+        <br>
+        Frecuencia Baja: Se realiza un bajo volumen de entrenamiento, con menos frecuencia en comparación con otras rutinas. Los entrenamientos pueden ser menos frecuentes, permitiendo un mayor tiempo de recuperación entre sesiones.</br>
+        <br>
+        Entrenamiento con Peso Pesado: La rutina prioriza el uso de pesos más pesados, realizando menos repeticiones pero con mayor carga. Esto se hace para maximizar la estimulación de las fibras musculares.</br>
+        <br>
+        Corto Tiempo de Entrenamiento: Cada sesión de entrenamiento es breve pero intensa, evitando sesiones prolongadas en el gimnasio. Se enfoca en la calidad del esfuerzo en lugar de la cantidad de tiempo invertido.</br>
+        <br>
+        Principio de Descanso Completo: Se enfatiza la importancia del descanso completo entre las sesiones de entrenamiento para permitir una recuperación adecuada y evitar el sobreentrenamiento.</br>
+        <br>
+        Principio de Progresión: Se busca progresar constantemente, ya sea aumentando el peso, la intensidad o el número de repeticiones, para asegurar una adaptación continua y un crecimiento muscular.</br>
+        <br>
+        Selección de Ejercicios Clave: La rutina se enfoca en ejercicios compuestos clave que trabajan grandes grupos musculares, minimizando el número total de ejercicios.</br>
+        <br>
+        Individualización: Se destaca la importancia de adaptar la rutina a las necesidades y capacidad individual de cada persona, reconociendo que no todos los cuerpos responden de la misma manera al entrenamiento.</p>
+
+        <img class="mikementzer" src="imagenes/mike.jpg" width="400px" height="450px">
+
+
+
+        <h1 class="arnold">Arnold Split</h1>
+        <p class="infoarnold">La "Arnold Split" es una rutina de entrenamiento clásica creada por el legendario fisicoculturista Arnold Schwarzenegger. Este enfoque se centra en el desarrollo equilibrado de todos los grupos musculares y se caracteriza por su enfoque en el volumen de entrenamiento y la frecuencia de entrenamiento.</p>
+        <h5 class="arnold2">Caracteristicas</h5>
+
+        <p class="infoarnold2">División de Rutina: La rutina se divide en diferentes días de entrenamiento, cada uno dedicado a grupos musculares específicos. La división clásica podría ser Pecho y Espalda, Piernas y Abdominales, Hombros y Brazos.</br>
+            <br>
+            Frecuencia de Entrenamiento: Se recomienda un enfoque de entrenamiento frecuente, a menudo entrenando cada grupo muscular dos veces por semana. Esto permite un mayor estímulo y crecimiento muscular a lo largo del tiempo.</br>
+            <br>
+            Volumen de Entrenamiento: La rutina Arnold Split destaca el uso de un volumen de entrenamiento significativo, con múltiples series y repeticiones. Se utiliza una combinación de ejercicios compuestos y aislados para abordar diferentes aspectos del desarrollo muscular.</br>
+            <br>
+            Ejercicios Completos y Variedad: Arnold favorecía ejercicios compuestos como press de banca, dominadas y sentadillas, pero también incorporaba una amplia variedad de ejercicios para garantizar un desarrollo completo y simétrico.</br>
+            <br>
+            Intensidad Controlada: Aunque se utiliza un enfoque de alta intensidad, la rutina Arnold Split no se basa en la extenuación constante. Se busca un equilibrio entre la intensidad y la capacidad de recuperación.</br>
+            <br>
+            Priorización del Pecho: Arnold Schwarzenegger era conocido por su desarrollo masivo en el pecho, y su rutina refleja una prioridad especial en este grupo muscular. Incluye ejercicios como press de banca y fondos.</br>
+            <br>
+            Abdominales y Cardio: Se recomienda la inclusión regular de ejercicios abdominales y sesiones de cardio para mantener un físico equilibrado y saludable.</br>
+            <br>
+            Flexibilidad en el Programa: Aunque hay una estructura básica, Arnold enfatizaba la importancia de adaptar la rutina según las necesidades y respuestas individuales, permitiendo cierta flexibilidad y ajustes según sea necesario.</p>
+
+
+        <img class="arnoldfoto" src="imagenes/arnold.jpg" width="400px" height="450px">
+
+
+
+        <h1 class="ppl">Push Pull Legs (PPL)</h1>
+        <p class="infoppl">La rutina "Push, Pull, Legs" (PPL) es un enfoque de entrenamiento dividido que organiza los ejercicios según los movimientos y grupos musculares involucrados.</p>
+        <h5 class="ppl2">Caracteristicas</h5>
+
+
+        <p class="infoppl2">
+
+            Push (Empuje): Incluye ejercicios que trabajan principalmente los músculos implicados en empujar, como pecho, hombros y tríceps.</br>
+            Pull (Tirón): Enfocado en ejercicios de tirar que involucran la espalda, bíceps y otros músculos relacionados.</br>
+            Legs (Piernas): Se concentra en ejercicios para el tren inferior, como sentadillas, peso muerto y ejercicios para cuádriceps e isquiotibiales.</br>
+            <br>
+            Frecuencia de Entrenamiento:</br>
+            
+            Cada sesión de entrenamiento (Push, Pull, Legs) se realiza por separado.</br>
+            La frecuencia recomendada es de 3 a 6 días por semana, dependiendo de la preferencia del individuo y su capacidad de recuperación.</br>
+            <br>
+            Volumen y Variedad:</br>
+            
+            Se busca un equilibrio entre el volumen de entrenamiento y la intensidad.
+            Incluye una variedad de ejercicios para cada grupo muscular, lo que permite trabajar diferentes ángulos y estimular el crecimiento muscular de manera integral.</br>
+            <br>
+            Intensidad y Progresión:</br>
+            
+            Se utiliza la progresión de carga para desafiar constantemente los músculos y promover el crecimiento.
+            La intensidad se mantiene alta, especialmente al acercarse al fallo muscular en los últimos sets.</br>
+            <br>
+            Flexibilidad y Adaptabilidad:</br>
+            
+            La rutina PPL es versátil y puede adaptarse a diferentes niveles de condición física y objetivos.
+            Puede ser modificada para incluir ejercicios específicos según las metas individuales, como fuerza, hipertrofia o resistencia.</br>
+            <br>
+            Incorporación de Ejercicios Compuestos y Aislados:</br>
+            
+            Incluye tanto ejercicios compuestos que trabajan varios grupos musculares como ejercicios aislados que se centran en músculos específicos.</br>
+            <br>
+            Descanso y Recuperación:</br>
+            
+            Se recomienda un adecuado tiempo de descanso entre las sesiones y una atención a la recuperación, especialmente dado que cada grupo muscular se entrena con menos frecuencia que en otros enfoques de entrenamiento.</br>
+            <br>
+            Cardio y Accesorios:</br>
+            
+            Puede combinarse con sesiones de cardio y accesorios específicos según las metas individuales.</p>
+
+
+</div>
+<footer class="fin">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li>
+            <a href="#" class="nav-link px-2 text-muted">Inicio</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link px-2 text-muted">Proteinas</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link px-2 text-muted">Aminoacidos</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link px-2 text-muted">Multivitaminicos</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link px-2 text-muted">Accesorios</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link px-2 text-muted">Nosotros</a>
+        </li>
+    </ul>
+    <p class="text-center text-muted">2024 GymShark</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
